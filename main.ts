eeac68b4b32febf430ee46bf995d4ee62f140b82
xuyuchen <xuyuchen@agile-china.com>
2024-08-16 09:00:38 +0800

    去除node等引用，改为手动输入参数

--- main.ts
+++ main.ts
@@ -1,12 +1,21 @@
-import assert from 'node:assert'
+/*
+ * @Descripttion: 
+ * @详细文档参考: 
+ * @version: 
+ * @Author: xuyuchen
+ * @Date: 2024-08-06 17:39:10
+ * @LastEditors: xuyuchen
+ * @LastEditTime: 2024-08-16 08:56:34
+ */
+// import assert from 'node:assert'
 import 'dotenv/config'
-import process from 'node:process'
+// import process from 'node:process'
 import { doAttendanceForAccount } from './src'
 
-assert(typeof process.env.SKLAND_TOKEN === 'string')
+// assert(typeof process.env.SKLAND_TOKEN === 'string')
 
-const accounts = Array.from(process.env.SKLAND_TOKEN.split(','))
-const withServerChan = process.env.SERVERCHAN_SENDKEY
-const withBark = process.env.BARK_URL
+const accounts = []
+// const withServerChan = process.env.SERVERCHAN_SENDKEY
+// const withBark = process.env.BARK_URL
 
-await Promise.all(accounts.map(token => doAttendanceForAccount(token, { withServerChan, withBark })))
+await Promise.all(accounts.map(token => doAttendanceForAccount(token, { withServerChan: false, withBark: false })))
